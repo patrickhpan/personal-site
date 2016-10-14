a26df3859ed603e1db8e9e4e61ce41dcc14dabd9
Patrick Pan <patrick.pan@patrickpan.com>
2016-10-13 21:16:09 -0400

    Unified styling on blog post

--- client/components/BlogPost.jsx
+++ client/components/BlogPost.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import marked from 'marked';
 
+import { renderItem } from './_Content';
+import keyify from '../js/keyify'
 import { getEntryBySlug } from '../js/contentful';
+import { footer } from '../json/BlogPost.json';
 
 class BlogPost extends React.Component {
     constructor() {
@@ -27,9 +31,14 @@ class BlogPost extends React.Component {
                 title: "Post not found."
             }
         }
+
+        let splitBody = post.fields.body.split(/\n+/g);
+
         return {
             title: post.fields.title,
-            md: post.fields.body
+            markdowns: splitBody.map(item => { return {
+                __html: marked(item)
+            }})
         }
     }
     render() {
@@ -38,11 +47,18 @@ class BlogPost extends React.Component {
             return null;
         }
 
-        let { title, md } = this.formatPost(this.state.post);
+        let { title, markdowns } = this.formatPost(this.state.post);
+
+        let content = keyify(markdowns.map(md => <div className="content-md" dangerouslySetInnerHTML={md} />));
+
+        let renderedFooter = keyify(renderItem(footer));
 
         return <div className="BlogPost"> 
             <h2>{title}</h2>
-            {md}
+            <div className="content-container">
+                {content}
+                {renderedFooter}
+            </div>
         </div>
     }
 }
---
+++ client/img/_images.js
@@ -0,0 +1,5 @@
+import me from "./me.jpg";
+
+export default {
+    me: me
+}
