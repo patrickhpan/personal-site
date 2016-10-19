6c8086400e8d0ec8f9bbc42a49bc9c018972d893
Patrick Pan <patrick.pan@patrickpan.com>
2016-10-19 18:44:57 -0400

    Began lightroom api

---
+++ server/apis/lightroom.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+const request = require('request-promise');
+const Promise = require('bluebird')
+const cache = require('./cache');
+
+const LIGHTROOM_SPACE = process.env.LIGHTROOM_SPACE;
+const LIGHTROOM_ALBUM = process.env.LIGHTROOM_ALBUM;
+
+function createLightroomURL(values) {
+    let url = 'https://lightroom.adobe.com/v2c';
+    values.forEach(value => {
+        if(typeof value === 'object') {
+            url += `/${value.key}/${value.value}`;
+        } else {
+            url += `/${value}`;
+        }
+    })
+    return url;
+}
+
+function parseResponse(data) {
+    data = data.split(/\n/g);
+    data.shift();
+    return JSON.parse(data[0]);
+}
+
+function getImages() {
+    let url = createLightroomURL([
+        {
+            key: 'spaces',
+            value: LIGHTROOM_SPACE
+        }, {
+            key: 'albums',
+            value: LIGHTROOM_ALBUM
+        }, 
+        'assets?subtype=image' 
+    ]);
+
+    return request({
+        url: url
+    })
+        .then(parseResponse)
+        .then(data => {
+            let ids = data.resources.map(x => x.links.self.href.replace(/^.+\//,''));
+            return ids.map(getAssets);
+        });
+}
+
+function getAssets(id) {
+    let url = createLightroomURL([
+        {
+            key: 'spaces',
+            value: LIGHTROOM_SPACE
+        },
+        {
+            key: 'assets',
+            value: id
+        }
+    ])
+
+    return request({
+        url: url
+    })
+        .then(parseResponse)
+        .then(data => {
+            let links = data.links;
+            return {
+                full: links['/rels/rendition_type/2048'].href,
+                small: links['/rels/rendition_type/640'].href,
+                thumbnail: links['/rels/rendition_type/thumbnail2x'].href
+            }
+        });
+}
+
+module.exports = {
+    getImages
+}
