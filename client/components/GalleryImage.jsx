15bb5673f8aecb820f26cbe94411f14c5a46cbcb
Patrick Pan <patrickpan@college.harvard.edu>
2016-10-28 02:32:17 -0400

    Added fade for image load effect

--- client/components/GalleryImage.jsx
+++ client/components/GalleryImage.jsx
@@ -1,23 +1,54 @@
 import React from 'react';
 
 class GalleryImage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            loaded: false
+        }
+    }
+    onImageLoad() {
+        this.setState({
+            loaded: true
+        })
+    }
     render() {
-        let src = this.props.src;
+        let src = this.props.src;        
+
+        let classList = ['image'];
         let style;
-        let classList = ['GalleryImage']; 
-        if (src) {
+        if (src && this.state.loaded) {
             style = {
                 backgroundImage: `url(${src})`
-            }
+            };
+            classList.push('loaded');
         } else {
             classList.push('loading')
         }
-
-        return <div 
+         
+        let img = <div
             className={classList.join(' ')}
             style={style}
-            onClick={this.props.onClick}
         />
+        
+        let onLoad = this.onImageLoad.bind(this)
+
+        let loader = src && !this.state.loaded ?
+            <img
+                src={src}
+                onLoad={onLoad}
+                className="hidden"
+            /> :
+            null;
+
+
+        return <div 
+            className='GalleryImage'
+            onClick={this.props.onClick}
+        >
+            {loader}
+            {img}
+        </div>    
     }
 }
 
